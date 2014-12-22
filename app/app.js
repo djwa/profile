0b35632052f2348273593d5462f5ee4223258389
djwa <hello@djwa.pl>
2014-12-22 21:28:53 +0000

    -a

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function () {
-    var app = angular.module('app', []);
+    var app = angular.module('app', ['']);
     app.factory('Data', function () {
-        return {message: 'my name is dupa, lord dupa'}
+        return {message: 'my name is dupa, lord dupa'};
     });
     app.controller('ProfilePage1', [function ($scope, Data) {
             $scope.data = Data;
