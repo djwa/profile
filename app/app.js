657527b7dd9c1dded047b292f2a7a232e685d1ce
djwa <hello@djwa.pl>
2014-12-25 19:14:14 +0000

    well done J!

--- app/app.js
+++ app/app.js
@@ -1,23 +1,20 @@
 (function () {
-<<<<<<< HEAD
-    var app = angular.module('app', ['']);
-    app.factory('Data', function () {
-        return {message: 'my name is dupa, lord dupa'};
-=======
     'use strict';
     var app = angular.module('app', []);
     app.factory('Data', function () {
         return {message: 'I made it works!'};
->>>>>>> a1cc27d364344281efd04b1d81f1ccfad7f114c9
     });
+
+    app.filter('reverse', function (Data) {
+        return function (text) {
+            return text.split("").reverse().join("") + " " + Data.message;
+        };
+    });
+
     app.controller('ProfilePage1', ['$scope', 'Data', function ($scope, Data) {
             $scope.data = Data;
         }]);
     app.controller('ProfilePage2', ['$scope', 'Data', function ($scope, Data) {
             $scope.data = Data;
-
-            $scope.reversedMessage = function (message) {
-                return message.split("").reverse().join("");
-            };
         }]);
 })();
