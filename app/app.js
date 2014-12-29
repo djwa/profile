f315de2ed54ca0d28f58613a4089b7fb108301f7
djwa <hello@djwa.pl>
2014-12-29 20:44:59 +0000

    -a

--- app/app.js
+++ app/app.js
@@ -1,20 +1,11 @@
 (function () {
     'use strict';
     var app = angular.module('app', []);
-    app.factory('Data', function () {
-        return {message: 'I made it works!'};
-    });
 
-    app.filter('reverse', function (Data) {
-        return function (text) {
-            return text.split("").reverse().join("") + " " + Data.message;
-        };
-    });
+    app.controller('jListCrtl', ['$http', '$scope', function ($http, $scope) {
+            $http.get('data/fellas.json').success(function(data){
+                $scope.fellas = data;
+            });
+        }]);
 
-    app.controller('ProfilePage1', ['$scope', 'Data', function ($scope, Data) {
-            $scope.data = Data;
-        }]);
-    app.controller('ProfilePage2', ['$scope', 'Data', function ($scope, Data) {
-            $scope.data = Data;
-        }]);
 })();
