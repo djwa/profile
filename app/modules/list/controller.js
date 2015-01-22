6c69c6a09964c3a0283b75618e3ba8f49dd845a6
Jeremi <hello@djwa.pl>
2015-01-22 16:21:57 +0000

    post

--- app/modules/list/controller.js
+++ app/modules/list/controller.js
@@ -12,13 +12,32 @@
             });
         }]);
     list.controller('List2Crtl', ['$http', '$scope', function ($http, $scope) {
+            //scope of visibility aspects
             $scope.pageClass = "listPage";
             $scope.gridColumn = 6;
             $scope.oldList = false;
             $scope.newList = true;
             $scope.currentList = 'new-list';
+            //scope of data
             $http.get('data/data-test.json').success(function (data) {
                 $scope.fellas = data;
             });
+
+            $scope.addNew = function () {
+                console.log('Adding the form scope!');
+
+                var userUpdate = {
+                    name: $scope.fella.name,
+                    surname: $scope.fella.surname,
+                    company: $scope.fella.company,
+                    email: $scope.fella.email,
+                    phone: $scope.fella.phone
+                };
+
+                $.scope.all.push(userUpdate);
+                $http.post('all', userUpdate);
+
+            };
+
         }]);
 })();
