9b80d0753c4a65017b4bf696a8116e2ade4fc5af
djwa <hello@djwa.pl>
2015-01-28 20:15:46 +0000

    mongoose post

--- app/modules/list/controller.js
+++ app/modules/list/controller.js
@@ -1,27 +1,25 @@
 (function () {
     var list = angular.module('listControllers', ['listDirectives']);
     //service
-    list.factory('fellaservice', ['$http', function ($http) {
-            //method1
-            return {
-                getData: function () {
-                    return $http({
-                        url: 'http://localhost:3000/fellas',
-                        method: 'GET'
-                    });
-                }
-            };
-
-//            return function () {
-//                this.initialize = function () {
-//                    var url = 'data/data-test.json';
-//                    $http.get(url).success(function(data){
-//                        fellas = data;
+//    list.factory('fellaservice', ['$http', function ($http) {
+//            //method1
+//            var url = 'http://localhost:3000/fellas';
+//            return {
+//                getData: function () {
+//                    return $http({
+//                        url: url,
+//                        method: 'GET'
 //                    });
-//                };
-//                this.initialize();
+//                },
+//                postData: function () {
+//                    return $http({
+//                        url: url,
+//                        method: 'POST',
+//                        data: 'fella'
+//                    });
+//                }
 //            };
-        }]);
+//        }]);
     //controllers
     list.controller('ListCrtl', ['$http', '$scope', function ($http, $scope) {
             $scope.pageClass = "listPage";
@@ -33,36 +31,60 @@
                 $scope.fellas = data;
             });
         }]);
-    list.controller('List2Crtl', ['$http', '$scope', 'fellaservice', function ($http, $scope, fellaservice) {
+    list.controller('List2Crtl', ['$http', '$scope', function ($http, $scope) { //, fellaservice
             //scope of visibility aspects
             $scope.pageClass = "listPage";
             $scope.gridColumn = 6;
             $scope.oldList = false;
             $scope.newList = true;
             $scope.currentList = 'new-list';
-
             //scope of data
 
             //method 1
-            $scope.fellas = [];
-            fellaservice.getData().success(function (data) {
-                $scope.fellas = data;
-                $scope.addNew = function () {
-                    console.log('Adding the form scope!');
-                    var userUpdate = {
-                        name: $scope.fella.name,
-                        surname: $scope.fella.surname,
-                        company: $scope.fella.company,
-                        email: $scope.fella.email,
-                        phone: $scope.fella.phone
-                    };
-                    data.push(userUpdate);
-                    $scope.msg = 'Data sent: ' + JSON.stringify(data);
-                };
-            });
+//            $scope.fellas = [];
+//            fellaservice.getData().success(function (data) {
+//                $scope.fellas = data;
+//            });
+//            $scope.addNew = function (fella) {
+//                fellaservice.postData(fella).success(function (data) {
+//                    console.log('Adding the form scope!', data);
+//                });
+////                var userUpdate = {
+////                    name: $scope.fella.name,
+////                    surname: $scope.fella.surname,
+////                    company: $scope.fella.company,
+////                    email: $scope.fella.email,
+////                    phone: $scope.fella.phone
+////                };
+////                data.push(userUpdate);
+////                $scope.msg = 'Data sent: ' + JSON.stringify(data);
+//            };
+
+
+            $http.get('http://localhost:3000/fellas')
+                    .success(function (data) {
+                        $scope.fellas = data;
+                    });
+
+
+            $scope.addNew = function (fella) {
+                $http.post('http://localhost:3000/fellas', fella)
+                        .success(function (data) {
+//                            console.log(data);
+//                            $scope.msg = 'Data sent?: ' + JSON.stringify(data);
+                            $scope.fellas = data;
+                        })
+                        .error(function () {
+                            $scope.newList = false;
+                            $scope.msg = "Sorry but there was a connection problem. Data has not been injected. :(";
+                        });
+            };
 
 
 
+
+
+//method 0
 //            $http.get('data/data-test.json').success(function (data) {
 //                $scope.fellas = data;
 //                $scope.addNew = function () {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require('mongoose');
 var cors = require("cors");
+var bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/felony');
 
@@ -15,11 +16,40 @@ var fellasSchema = {
 var Fella = mongoose.model('Fella', fellasSchema, 'fellas');
 
 var app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/fellas', function (req, res) {
     Fella.find(function (err, doc) {
         res.send(doc);
     });
+});
+//app.post('/fellas', function (req, res) {
+////        res.send(req.body);
+//        
+//        data.push(req.body);
+//        res.send(data);
+//});
+
+
+app.post('/fellas', function (req, res) {
+    var Fella;
+    console.log("POST: ");
+    console.log(req.body);
+    Fella = new FellaModel({
+        name: req.body.name,
+        surname: req.body.surname,
+        company: req.body.company,
+        email: req.body.email,
+        phone: req.body.phone
+    });
+    Fella.save(function (err) {
+        if (!err) {
+            return console.log("created");
+        } else {
+            return console.log(err);
+        }
+    });
+    return res.send(fella);
 });
 app.listen(3000);
