281bbdd7b05b659c984a7539df638d9cb16243cf
djwa <hello@djwa.pl>
2015-02-13 13:45:37 +0000

    list service

--- app/scripts/list/controller.js
+++ app/scripts/list/controller.js
@@ -1,34 +1,5 @@
 (function () {
-    var list = angular.module('listControllers', ['listDirectives']);
-    //service
-    list.factory('fellaservice', ['$http', function ($http) {
-        var listurl = 'http://localhost:3000/fellas';
-        var listservice = {
-
-            getData: function () {
-                return $http({
-                    method: 'GET',
-                    url: listurl
-                });
-            },
-            postData: function (fella) {
-                return $http({
-                    method: 'POST',
-                    url: listurl,
-                    data: JSON.stringify(fella)
-                });
-            },
-            removeData: function (id) {
-                return $http({
-                    method: 'DELETE',
-                    url: listurl + '/' + id,
-                    data: 'fella'
-                });
-            }
-        };
-        return listservice;
-    }]);
-    //controllers
+    var list = angular.module('listControllers', ['listDirectives','listService']);
     list.controller('ListCrtl', ['$http', '$scope', function ($http, $scope) {
             $scope.pageClass = "listPage";
             $scope.gridColumn = 12;
---
+++ app/scripts/list/service.js
@@ -0,0 +1,29 @@
+var listService = angular.module('listService',[]);
+
+listService.factory('fellaservice', ['$http', function ($http) {
+    var listurl = 'http://localhost:3000/fellas';
+    var listservice = {
+
+        getData: function () {
+            return $http({
+                method: 'GET',
+                url: listurl
+            });
+        },
+        postData: function (fella) {
+            return $http({
+                method: 'POST',
+                url: listurl,
+                data: JSON.stringify(fella)
+            });
+        },
+        removeData: function (id) {
+            return $http({
+                method: 'DELETE',
+                url: listurl + '/' + id,
+                data: 'fella'
+            });
+        }
+    };
+    return listservice;
+}]);
