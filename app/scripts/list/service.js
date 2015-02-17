851926af46f67ab3125dc9365cb2ad709bcbb640
djwa <hello@djwa.pl>
2015-02-17 23:35:58 +0000

    z koziej dupy

--- app/scripts/list/service.js
+++ app/scripts/list/service.js
@@ -1,29 +1,33 @@
-var listService = angular.module('listService',[]);
+define(['./module'], function () {
 
-listService.factory('fellaservice', ['$http', function ($http) {
-    var listurl = 'http://localhost:3000/fellas';
-    var listservice = {
+    var listService = angular.module('listService', []);
 
-        getData: function () {
-            return $http({
-                method: 'GET',
-                url: listurl
-            });
-        },
-        postData: function (fella) {
-            return $http({
-                method: 'POST',
-                url: listurl,
-                data: JSON.stringify(fella)
-            });
-        },
-        removeData: function (id) {
-            return $http({
-                method: 'DELETE',
-                url: listurl + '/' + id,
-                data: 'fella'
-            });
-        }
-    };
-    return listservice;
-}]);
+    listService.factory('fellaservice', ['$http', function ($http) {
+        var listurl = 'http://localhost:3000/fellas';
+        var listservice = {
+
+            getData: function () {
+                return $http({
+                    method: 'GET',
+                    url: listurl
+                });
+            },
+            postData: function (fella) {
+                return $http({
+                    method: 'POST',
+                    url: listurl,
+                    data: JSON.stringify(fella)
+                });
+            },
+            removeData: function (id) {
+                return $http({
+                    method: 'DELETE',
+                    url: listurl + '/' + id,
+                    data: 'fella'
+                });
+            }
+        };
+        return listservice;
+    }]);
+
+});
