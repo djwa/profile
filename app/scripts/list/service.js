1d97c0484862449bd1543adab560eaeba52148ae
Jeremi <hello@djwa.pl>
2015-02-18 15:32:44 +0000

    service and controller injected properly

--- app/scripts/list/service.js
+++ app/scripts/list/service.js
@@ -1,29 +1,36 @@
-var listService = angular.module('listService',[]);
+define(['./module'], function (module) {
 
-listService.factory('fellaservice', ['$http', function ($http) {
-    var listurl = 'http://localhost:3000/fellas';
-    var listservice = {
+    Service.$inject = [
+        '$http'
+    ];
 
-        getData: function () {
-            return $http({
-                method: 'GET',
-                url: listurl
-            });
-        },
-        postData: function (fella) {
-            return $http({
-                method: 'POST',
-                url: listurl,
-                data: JSON.stringify(fella)
-            });
-        },
-        removeData: function (id) {
-            return $http({
-                method: 'DELETE',
-                url: listurl + '/' + id,
-                data: 'fella'
-            });
-        }
-    };
-    return listservice;
-}]);
+    function Service($http) {
+        var listurl = 'http://localhost:3000/fellas';
+        var listservice = {
+            getData: function () {
+                return $http({
+                    method: 'GET',
+                    url: listurl
+                });
+            },
+            postData: function (fella) {
+                return $http({
+                    method: 'POST',
+                    url: listurl,
+                    data: JSON.stringify(fella)
+                });
+            },
+            removeData: function (id) {
+                return $http({
+                    method: 'DELETE',
+                    url: listurl + '/' + id,
+                    data: 'fella'
+                });
+            }
+        };
+        return listservice;
+    }
+
+    module.factory('fellaservice', Service);
+
+});
