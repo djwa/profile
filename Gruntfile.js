eb009c4a2abecdd4f92fbc4dcc9a3b44f5774fa9
Jeremi <hello@djwa.pl>
2015-02-26 12:56:56 +0000

    gruntfilejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,23 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     require('time-grunt')(grunt);
 
-    var config = {
-        appConfig: {
-            app: require('./bower.json').appPath || 'app',
-            dist: 'dist'
-        }
-    };
-    config = require('load-grunt-configs')(grunt, config);
-    grunt.initConfig(config);
+//    var config = {
+//        appConfig: {
+//            app: require('./bower.json').appPath || 'app',
+//            dist: 'dist'
+//        }
+//    };
+//    config = require('load-grunt-configs')(grunt, config);
+//    grunt.initConfig(config);
+
+
+//https://www.youtube.com/watch?v=gIbfDxF69c8
+//https://www.youtube.com/watch?v=Jgm4dV8-Xv0
+
+    grunt.initConfig({
+
+    });
+
 
     grunt.registerTask('build', [
         'copy:dist'
