e72c2d2e8e4451c05345b03dd434b017d9f96039
Jeremi <hello@djwa.pl>
2015-03-05 12:41:03 +0000

    controller changed for the requirejs way

--- app/scripts/common/controller.js
+++ app/scripts/common/controller.js
@@ -1,12 +1,24 @@
-define(['./module'], function(controllers){
+define(['./module'], function (module) {
     'use strict';
 
-    controllers.controller('PathsCrtl', ['$scope', function ($scope) {
+    Controller1.$inject = [
+        '$scope'
+    ];
+    Controller2.$inject = [
+        '$scope',
+        '$location'
+    ];
+
+    function Controller1($scope) {
         $scope.LogoImgPath = "http://djwa.pl/angular-logo.png";
-    }]);
-    controllers.controller('SiteLocationCrtl', ['$scope', '$location', function ($scope, $location) {
+    }
+
+    function Controller2($scope, $location) {
         $scope.isActive = function (route) {
             return route === $location.path();
         };
-    }]);
+    }
+
+    module.controller('PathsCrtl', Controller1);
+    module.controller('SiteLocationCrtl', Controller2);
 });
