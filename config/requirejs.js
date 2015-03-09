125e3a59023e96a778a4f29b5b49a7d5368519c0
djwa <hello@djwa.pl>
2015-03-09 17:11:30 +0000

    lf

--- config/requirejs.js
+++ config/requirejs.js
@@ -5,7 +5,6 @@ module.exports = function (grunt, appConfig) {
                 uglify: {},
                 baseUrl: '<%= appConfig.app %>/scripts',
                 mainConfigFile: '<%= appConfig.app %>/scripts/entrypoint.js',
-//                generateSourceMaps: appConfig.env.generateSourceMaps,
                 name: 'entrypoint',
                 out: '<%= appConfig.dist %>/scripts/main.min.js'
             }
