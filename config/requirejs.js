a13a8d1908ad1d3d6e3202994e8aff82273569cf
Jeremi <hello@djwa.pl>
2015-03-20 13:21:35 +0000

    generated source maps

--- config/requirejs.js
+++ config/requirejs.js
@@ -2,35 +2,18 @@ module.exports = function (grunt, appConfig) {
     return {
         compile: {
             options: {
-                uglify: {},
+                uglify2: {
+                    mangle: false
+                },
                 baseUrl: '<%= appConfig.app %>/scripts',
                 mainConfigFile: '<%= appConfig.app %>/scripts/entrypoint.js',
+                optimize: 'uglify2',
+                generateSourceMaps: true,
                 name: 'entrypoint',
-                out: '<%= appConfig.dist %>/scripts/main.min.js'
+                out: '<%= appConfig.dist %>/scripts/main.min.js',
+                preserveLicenseComments: false,
+                findNestedDependencies: true
             }
         }
     }
 };
-// todo: fix the paths problem
-//module.exports = function(grunt, appConfig) {
-//    return {
-//        compile: {
-//            options: {
-//                uglify2: {
-//                    mangle: false
-//                },
-//                baseUrl: '<%= appConfig.app %>/scripts',
-//                mainConfigFile: '<%= appConfig.app %>/scripts/entrypoint.js',
-//                optimize: appConfig.env.minimizeJs ? 'uglify2' : 'none',
-//                generateSourceMaps: appConfig.env.generateSourceMaps,
-//                name: 'entrypoint',
-//                out: '<%= appConfig.dist %>/scripts/main.min.js',
-//                preserveLicenseComments: false,
-//                findNestedDependencies: true,
-//                rawText: {
-//                    'Env': '<%= env.amdModule %>'
-//                }
-//            }
-//        }
-//    }
-//};
